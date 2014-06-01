1707c73ff0b4ee18dec4d744e75fb3d16131f073
Dênis <Dnis@Denis-PC>
2014-06-01 20:39:18 -0300

    Alterações no Inserir Curso

--- src/java/servlets/ServInserirCurso.java
+++ src/java/servlets/ServInserirCurso.java
@@ -34,6 +34,7 @@ public class ServInserirCurso extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        doPost(request, response);
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
